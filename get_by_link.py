2aca8e97f593e72604a008c9248779c0842b0b73
ABekkaliyev <ABekkaliyev@beeline.kz>
2022-12-11 21:17:39 +0600

    testing with node

--- get_by_link.py
+++ get_by_link.py
@@ -4,7 +4,6 @@ import random
 from utils import get_random_header
 
 def get_prod_by_link(url):
-    
     session = requests.Session()
     response = session.get(
             url, 
@@ -14,7 +13,7 @@ def get_prod_by_link(url):
     start, end = 'window.__INIT_DATA=', '</script>'
     res = json.loads(body.split(start)[-1].split(end)[0])
     
-    # print()
+    # print(json.dumps(res, indent=4, ensure_ascii=False))
     return json.dumps(res, indent=4, ensure_ascii=False)
 
 # url = f'https://detail.1688.com/offer/627534969028.html?spm=a26352.13672862.offerlist.20.40d82ab97Hn8h2&cosite=-&tracelog=p4p&_p_isad=1&clickid=ec66899442184bc6b4473873183770bf&sessionid=094d711655c4d7471291943a7d12e7ca'
--- test_main.py
+++ test_main.py
@@ -1,4 +1,7 @@
+import sys
+
 from get_by_link import get_prod_by_link
 
-url = f'https://detail.1688.com/offer/627534969028.html?spm=a26352.13672862.offerlist.20.40d82ab97Hn8h2&cosite=-&tracelog=p4p&_p_isad=1&clickid=ec66899442184bc6b4473873183770bf&sessionid=094d711655c4d7471291943a7d12e7ca'
+url = sys.argv[1]
+# url = f'https://detail.1688.com/offer/627534969028.html?spm=a26352.13672862.offerlist.20.40d82ab97Hn8h2&cosite=-&tracelog=p4p&_p_isad=1&clickid=ec66899442184bc6b4473873183770bf&sessionid=094d711655c4d7471291943a7d12e7ca'
 print(get_prod_by_link(url))
