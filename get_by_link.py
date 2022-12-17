64904a044c0ec7ce78d3b2bdfbc6fe2356b90a0d
ABekkaliyev <ABekkaliyev@beeline.kz>
2022-12-17 20:49:59 +0600

    getting details

--- get_by_link.py
+++ get_by_link.py
@@ -1,32 +1,14 @@
-# import requests
-# import json
-# import random
-# from utils import get_random_header
-
-# def get_prod_by_link(url):
-#     session = requests.Session()
-#     response = session.get(
-#             url, 
-#             verify=False, 
-#             headers=get_random_header()).text
-#     body = response.split('<script>')[6]
-#     start, end = 'window.__INIT_DATA=', '</script>'
-#     res = json.loads(body.split(start)[-1].split(end)[0])
-    
-#     # print(json.dumps(res, indent=4, ensure_ascii=False))
-#     return json.dumps(res, indent=4, ensure_ascii=False)
-
-# # url = f'https://detail.1688.com/offer/627534969028.html?spm=a26352.13672862.offerlist.20.40d82ab97Hn8h2&cosite=-&tracelog=p4p&_p_isad=1&clickid=ec66899442184bc6b4473873183770bf&sessionid=094d711655c4d7471291943a7d12e7ca'
-# # print(get_prod_by_link(url))
-
 import requests
 import json
 import random
 from utils import get_random_header
 import sys 
 
+def get_details(details):
+    return [{'name': d['name'], 'values': d['values']} for d in details]
+
 def get_prod_by_link(url):
-    url = sys.argv[1]
+    # url = sys.argv[1]
     session = requests.Session()
     response = session.get(
             url, 
@@ -42,6 +24,8 @@ def get_prod_by_link(url):
             try:
                 if value['componentType'] == '@ali/tdmod-od-pc-offer-price':
                     product['prices'] = value['data']['priceModel']['currentPrices']
+                elif value['componentType'] == '@ali/tdmod-od-pc-attribute-new':
+                    product['details'] = get_details(value['data'])
             except KeyError or TypeError:
                 continue
     
@@ -54,10 +38,13 @@ def get_prod_by_link(url):
         product['images'] = imageList
     
     product['title'] = res['globalData']['tempModel']['offerTitle']
-    
-    return(json.dumps(product, indent=4, ensure_ascii=True))
+    # return(json.dumps(product, indent=4, ensure_ascii=True))
 
 
-if name == '__main__':
+if __name__ == '__main__':
     url = sys.argv[1]
-    print(get_prod_by_link(url))
+    # url = 'https://detail.1688.com/offer/692256621725.html?spm=a26352.13672862.offerlist.30.714b32c0fwVgA8&cosite=-&tracelog=p4p&_p_isad=1&clickid=bd938112b7744611977c680aef698af6&sessionid=893d0c70911a57cdd0f7b3b1621d375c'
+    # print(get_prod_by_link(url))
+    res = get_prod_by_link(url)
+    # with open("sample_2.json", "w") as outfile:
+        # outfile.write(json.dumps(res, indent=4, ensure_ascii=False))
