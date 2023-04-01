f4ff5b3c30effec2ece1884b8eea10eaae3560eb
Assan Bekkaliyev <47390497+asnbkk@users.noreply.github.com>
2023-04-01 15:35:42 +0600

    new file name for config.py

---
+++ proxy/generate_proxy.py
@@ -0,0 +1,19 @@
+import random
+
+host_list = [
+    '212.116.246.202', '91.147.126.179', '185.120.78.201', '46.8.31.101',
+    '109.248.199.58', '185.231.244.18', '89.191.227.27', '109.107.164.240'
+]
+
+USERNAME = 'bekkaliyevassan'
+PASS = 'JgMqRpcssL'
+PORT = '49155'
+
+
+def get_proxy():
+    HOST = random.choice(host_list)
+    proxy = {
+        'http': f'http://{USERNAME}:{PASS}@{HOST}:{PORT}',
+        'https': f'http://{USERNAME}:{PASS}@{HOST}:{PORT}'
+    }
+    return proxy
