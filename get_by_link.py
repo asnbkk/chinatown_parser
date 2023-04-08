9f5d2d541551af14351c3c9f1edbbcf9ef237632
Assan Bekkaliyev <47390497+asnbkk@users.noreply.github.com>
2023-04-08 13:50:33 +0600

    shit code

--- get_by_link.py
+++ get_by_link.py
@@ -7,35 +7,36 @@ import sys
 
 def get_variats(props, attrs, sku_model):
     variant_list = []
-    for i in props[0]['value']:
+    for i in props[0]["value"]:
         attr_list = []
         for attr in attrs:
-            if i['name'] in attr:
+            if i["name"] in attr:
                 prod = attrs[attr]
                 try:
-                    price = prod['price']
+                    price = prod["price"]
                 except:
                     # seems to be default price for all vars
                     # todo: price can be shit -
-                    price = sku_model['skuPriceScale']
+                    price = sku_model["skuPriceScale"]
 
-                if '-' in price:
-                    price = price.split('-')[-1]
+                if "-" in price:
+                    price = price.split("-")[-1]
 
-                name = prod['specAttrs'].split(';')[-1]
+                name = prod["specAttrs"].split(";")[-1]
                 # print('shit schema')
                 res_ = {
-                    'price': price,
-                    'name': name,
-                    'saleCount': prod['saleCount'],
-                    'canBookCount': prod['canBookCount']}
+                    "price": price,
+                    "name": name,
+                    "saleCount": prod["saleCount"],
+                    "canBookCount": prod["canBookCount"],
+                }
                 attr_list.append(res_)
-        variant_list.append({**i, 'subvariants': attr_list})
+        variant_list.append({**i, "subvariants": attr_list})
     return variant_list
 
 
 def get_details(details):
-    return [{'name': d['name'], 'values': d['values']} for d in details]
+    return [{"name": d["name"], "values": d["values"]} for d in details]
 
 
 def get_prod_by_link(url, CERT_PATH):
@@ -48,60 +49,91 @@ def get_prod_by_link(url, CERT_PATH):
         verify=CERT_PATH,
         headers=get_random_header(),
         proxies=get_proxy(),
-        timeout=10).text
+        timeout=10,
+    ).text
 
-    body = response.split('<script>')[6]
-    start, end = 'window.__INIT_DATA=', '</script>'
+    body = response.split("<script>")[6]
+    start, end = "window.__INIT_DATA=", "</script>"
     res = json.loads(body.split(start)[-1].split(end)[0])
 
     product = {}
 
-    sku_model = res['globalData']['skuModel']
-    props = sku_model['skuProps']
-    attrs = sku_model['skuInfoMap']
-    product['variants'] = get_variats(props, attrs, sku_model)
+    product["prices"] = res["globalData"]["orderParamModel"]["orderParam"]["skuParam"]
+    product["title"] = res["globalData"]["tempModel"]["offerTitle"]
+    subvar_product_amount = res["globalData"]["orderParamModel"]["orderParam"]
 
+    sku_model = res["globalData"]["skuModel"]
     try:
-        product['variant_name'] = props[0]['prop']
-        product['subvariant_name'] = props[1]['prop']
-    except:
-        product['subvariant_name'] = props[0]['prop']
-        product['variant_name'] = 'default'
+        props = sku_model["skuProps"]
+        attrs = sku_model["skuInfoMap"]
+        product["variants"] = get_variats(props, attrs, sku_model)
 
-    product['prices'] = res['globalData']['orderParamModel']['orderParam']['skuParam']
+        try:
+            product["variant_name"] = props[0]["prop"]
+            product["subvariant_name"] = props[1]["prop"]
+        except:
+            product["subvariant_name"] = props[0]["prop"]
+            product["variant_name"] = "default"
+    except KeyError as exception:
+        print(exception)
+        print("no sku props")
+        product["variant_name"] = "default"
+        product["subvariant_name"] = "default_1"
+        # shit subvar price
+        sub_price = product["prices"]["skuRangePrices"]["price"]
+        product["variants"] = [
+            {
+                "imageUrl": "",
+                "name": product["title"],
+                "subvariants": [
+                    {
+                        "price": sub_price,
+                        "name": product["title"],
+                        "saleCount": subvar_product_amount["saledCount"],
+                        "canBookCount": subvar_product_amount["canBookedAmount"],
+                    }
+                ],
+            }
+        ]
 
-    for key, value in res['data'].items():
+    for key, value in res["data"].items():
         if type(value) is not bool and type(value) is not list:
             try:
                 # if value['componentType'] == '@ali/tdmod-od-pc-offer-price':
 
-                if value['componentType'] == '@ali/tdmod-od-pc-attribute-new':
-                    product['details'] = get_details(value['data'])
+                if value["componentType"] == "@ali/tdmod-od-pc-attribute-new":
+                    product["details"] = get_details(value["data"])
             except KeyError or TypeError:
                 continue
 
     imageList = []
-    for image in res['globalData']['images']:
-        imageList.append({
-            'url': image['fullPathImageURI'],
-            'preview': image['size220x220ImageURI'],
-        })
-    product['images'] = imageList
+    for image in res["globalData"]["images"]:
+        try:
+            imageList.append(
+                {
+                    "url": image["fullPathImageURI"],
+                    "preview": image["size220x220ImageURI"],
+                }
+            )
+        except:
+            print("no image")
+    product["images"] = imageList
 
-    product['title'] = res['globalData']['tempModel']['offerTitle']
-    product['saled_count'] = res['globalData']['tempModel']['saledCount']
+    product["title"] = res["globalData"]["tempModel"]["offerTitle"]
+    product["saled_count"] = res["globalData"]["tempModel"]["saledCount"]
     # print(product)
-    return (json.dumps(product, indent=4, ensure_ascii=False))
+    return json.dumps(product, indent=4, ensure_ascii=False)
     # return res
 
 
-if __name__ == '__main__':
-    CERT_PATH = './proxy/zyte-proxy-ca.crt'
-    # url = 'https://detail.1688.com/offer/609602652530.html?spm=a26352.13672862.offerlist.33.38221e62IWOkdJ&cosite=-&tracelog=p4p&_p_isad=1&clickid=3bd2518aaed2456d98343f6429a046b4&sessionid=041a0a524fdef3f3136060143ee800cb'
+if __name__ == "__main__":
+    CERT_PATH = "./proxy/zyte-proxy-ca.crt"
+    url = "https://detail.1688.com/offer/706710213777.html"
+    612579417533
 
-    url = sys.argv[1]
-    CERT_PATH = sys.argv[2]
-
+    # url = sys.argv[1]
+    # CERT_PATH = sys.argv[2]
+    CERT_PATH = "./proxy/zyte-proxy-ca.crt"
     res = get_prod_by_link(url, CERT_PATH)
     print(res)
     # with open("sample_1.json", "w") as outfile:
--- get_rating.py
+++ get_rating.py
@@ -1,53 +1,42 @@
 import requests
+from proxy.generate_proxy import get_proxy
 from utils import get_random_header
 
-# header = get_random_header()
+url = 'https://detail.1688.com/offer/695187062527.html?spm=a260k.dacugeneral.home2019rec.23.663335e41iffsi&&scm=1007.21237.280932.0&pvid=cd9ef994-5c88-44ef-a97c-4d4b574c45f5&object_id=695187062527&udsPoolId=2274586&resourceId=1797996&resultType=normal'
+params = {
+    'jsv': '2.7.0',
+    'appKey': '12574478',
+    't': '1680431101627',
+    'sign': '7b71ecee72c923c238cebb776f902de2',
+    'api': 'mtop.mbox.fc.common.gateway',
+    'v': '1.0',
+    'type': 'json',
+    'isSec': '0',
+    'data': '{"fcGroup":"offer-cbu","fcName":"offerdetail-service","fcArgs":"{\\"serviceName\\":\\"offerSatisfactionService\\",\\"params\\":{\\"memberId\\":\\"b2b-332428333580fdb\\",\\"sellerId\\":3324283335,\\"offerId\\":570204643781,\\"isSignedForTm\\":true}}"}'
+}
 
-URL = 'https://h5api.m.1688.com/h5/mtop.mbox.fc.common.gateway/1.0/?jsv=2.4.11&appKey=12574478&t=1675580836117&sign=1a6fa5c789e55246f7bf48885250f881&api=mtop.mbox.fc.common.gateway&v=1.0&type=jsonp&isSec=0&timeout=20000&dataType=jsonp&callback=mtopjsonp5&data={"fcGroup":"offer-cbu","fcName":"offerdetail-service","fcArgs":"{\"serviceName\":\"offerSatisfactionService\",\"params\":{\"memberId\":\"b2b-2256983270\",\"sellerId\":2256983270,\"offerId\":656994546410,\"isSignedForTm\":false}}"}'
+header = get_random_header()
+header.update({'Referer': 'https://detail.1688.com/',
+              'Host': 'h5api.m.1688.com'})
 
 cookies = {
-    '__cn_logon__': "false",
-    '__cn_logon__.sig': "i6UL1cVhdIpbPPA_02yGiEyKMeZR2hBfnaoYK1CcrF4",
-    '_m_h5_c': "3ffea5c1c55b7176ca9c1a0465f0a6f8_1675531887824;f2e64eed8293b64664ac4a82b9e18648",
-    '_csrf_token': "1675522233271",
-    '_m_h5_tk': "32c134aba3ca7504f05fb93b1cd41463_1675589407504",
-    '_m_h5_tk_enc': "0c2868388bc0020ae59d40cb3889a6b2",
-    '_tb_token_': "e43e73eeea815",
-    'ali_ab': "5.251.148.84.1671928062683.6",
-    'ali-ss': "eyJ1c2VySWQiOm51bGwsImxvZ2luSWQiOm51bGwsInNpZCI6bnVsbCwiZWNvZGUiOm51bGwsIm1lbWJlcklkIjpudWxsLCJfZXhwaXJlIjoxNjc1MzIyNzA1OTU1LCJfbWF4QWdlIjo4NjQwMDAwMH0=",
-    'ali-ss.sig': "Rgn5tdbxmRnFM7OBC6SOcpksX7fgUr134Ba_eY0XuhY",
-    'alicnweb': "touch_tb_at=1675580785032",
-    'cna': "sGULHEGFlRkCAV+Nj+XSYh8G",
-    'cookie2': "1a8521126d6936f204cfe1a920547bf9",
-    'ctoken': "XQAFwoMxd1qXQllH7iAcnaga",
-    'isg': "BIqKeMTG3Rply1HtM5xmuxHz2HYsew7VmkP6uRTDNl1rxyqB_Ate5dAx16sbLIZt",
-    'keywordsHistory': "toy",
-    'l': "fBjmfrLuTKkf61YFfOfanurza77OSIOYYuPzaNbMi9fP_zCM50rfW6Jh_cTHC36NFsNkR3-uIOvDBeYBcnX7m72YBXViYDkmndLHR35....",
-    't': "ce18c59350caa7edf285964f2b093c7c",
-    'tfstk': "c4ARBjfj7qURwIB5u_H0LEKtTaEdZtURczsU9C-v3YbT1MFOitAM667nFwIN2qC..",
-    'x5secdata': "xbb03ea73843d5ce8b83c088f4546030931675236536a3362248a1620252218abczc2eaa__bx__h5api.m.1688.com:443/h5/mtop.alibaba.alisite.cbu.server.moduleasyncservice/1.0",
-    'xlly_s': "1"}
-
-# PROXY = '03.69.149.19:80'
-
-# PROXY = {'https': f'http://{PROXY}',
-#         'http': f'http://{PROXY}'}
-HEADER = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Connection': 'keep-alive',
-    'Host': 'h5api.m.1688.com',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'cross-site',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0'
+    '__cn_logon__': 'false',
+    '__mwb_logon_id__': 'undefined',
+    '_csrf_token': '_csrf_token',
+    '_m_h5_tk_enc': 'e2c363e88d61b11975e87e0ab36db433',
+    '_m_h5_tk': 'd38a5e54ef70b6b8d0fd62167eefa134_1680446026799',
+    '_tb_token_': 'e3e7ee4abe64a',
+    'cna': 'pl2IHFhM+BYCAV+Nj+WXSAJ5',
+    'cookie2': '186bcb57ebeddb9d49552404485c7bf1',
+    'isg': 'BAYG5EeHnjgl3Ep0TztlaJ8vVPqIZ0ohhhexkvAv4ikE86YNWPHXMRXLy7c_wEI5',
+    'l': 'fB_f0VMrNQcoTEGEBOfanurza77O7IOYYuPzaNbMi9fP_k1W5Nf1W1i_M58XCn6NFs6MR3-uIn39BeYBc61InxvO7VQ4dJMmndLHR35..',
+    'tfstk': 'cfycB0wdC4TBorqqNrMfLR2QwreGafvZ-RPUUgF27_2KPGwr4sfyaBkAjgmORZR1.',
+    't': '145a56430e3510acb629c1e0a9ba3ba7',
+    'xlly_s': '1'
 }
-r = requests.get(
-    URL, 
-    cookies=cookies,
-    headers=HEADER
-)
+
+r = requests.get(url,
+                 proxies=get_proxy(),
+                 verify='./proxy/zyte-proxy-ca.crt')
 
 print(r.text)
